901f4a085cf3e95cc5ea31adc86e876e80d46c3c
RYOTA-F <ry.fj0304@gmail.com>
2022-08-01 07:31:37 +0900

    [add] Link

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Test from '../../components/Test'
+import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
     <>
       <h1>About</h1>
       <Test />
+      <Link to="/">Go to Home</Link>
     </>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  return <h1>Home</h1>
+  return (
+    <>
+      <h1>Home</h1>
+      <Link to="/about">Go to About</Link>
+    </>
+  )
 }
 
 export default Home
